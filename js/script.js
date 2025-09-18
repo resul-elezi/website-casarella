b2d95ea31ec90e6e5482326741b9334f6f842d69
Resul Elezi <resul_elezi@protonmail.com>
2025-09-18 07:28:51 +0200

    #

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 'use strict';
 
 const headerImg = document.getElementById('header');
+const headerImgSrc = ['/img/headerbild.png', '/img/headerbild02.jpg', '/img/headerbild03.jpg', '/img/headerbild04.jpg'];
 
-function headerBildAustausch(headerImg, source) {
-    headerImg.src = source;
+let index = 0;
+
+function headerBildAustausch() {
+    headerImg.src = headerImgSrc[index];
+    index++;
+    // if(index >= headerImgSrc.length) {
+    //     index = 0;
+    // }
+    // index = index % headerImgSrc.length;
+    index = index >= headerImgSrc.length ? 0 : index;
 }
+
+const headerImgIdChange = setInterval(headerBildAustausch, 4000);
